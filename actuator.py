c4f7dd4553f03fd61065f9fa476d9d9a641c352a
neuronaddict <dev@lacourarie.org>
2019-05-13 14:30:12 +0200

    One endpoint mode.
    Possibility to test only a specific endpoint.

--- actuator.py
+++ actuator.py
@@ -5,24 +5,36 @@ import requests
 import argparse
 
 parser = argparse.ArgumentParser('scan various endpoints on spring boot')
+parser.add_argument('--proxy', help='proxy to use')
+parser.add_argument('--endpoint', help='handle specific endpoint')
 parser.add_argument('url', help='full url with path')
 
 args = parser.parse_args()
 
-with open('endpoints.txt', 'rt') as file:
-    endpoints = file.readlines()
 
 
-def test(endpoint_):
-    r = requests.get('{}/{}'.format(args.url, endpoint_))
+
+def test(endpoint):
+    r = requests.get('{}/{}'.format(args.url, endpoint), proxies={'http': args.proxy, 'https': args.proxy})
     json_response = r.json()
 
     if r.status_code == 200:
+        print('[+] get endpoint {}'.format(endpoint))
         pretty = json.dumps(json_response, indent=4, sort_keys=True)
 
-        with open('out/{}.json'.format(endpoint_), 'wt') as output_file:
+        filename = 'out/{}.json'.format(endpoint)
+        with open(filename, 'wt') as output_file:
             output_file.write(pretty)
+            print('[*] writed in {}'.format(filename))
+    else:
+        print('[-] bad response for endpoint {} : {}'.format(endpoint, r.status_code))
 
 
-for endpoint in endpoints:
-    test(endpoint)
+if args.endpoint is not None:
+    test(args.endpoint)
+else:
+    with open('endpoints.txt', 'rt') as file:
+        for line in file:
+            line = line.rstrip('\n')
+            test(line)
+
